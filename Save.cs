65beeb1bb42af994145e4e392085d863e2be5ba8
Денис <valchukdenis@mail.ru>
2023-10-20 00:28:08 +0300

    Реализация контроллера для содержания данных о сохранении

---
+++ Save.cs
@@ -0,0 +1,26 @@
+﻿namespace Laba
+{
+    internal class Save
+    {
+
+        private static string Seed;
+        private static string Nick1;
+        private static string Nick2;
+
+        public static string Information
+        {
+            get { return Seed; }
+            set { Seed = value; }
+        }
+        public static string Player1
+        {
+            get { return Nick1; }
+            set { Nick1 = value; }
+        }
+        public static string Player2
+        {
+            get { return Nick2; }
+            set { Nick2 = value; }
+        }
+    }
+}
