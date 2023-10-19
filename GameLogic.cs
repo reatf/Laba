154730da537018bcf6b9f922fdf847405e92ac5c
Денис <valchukdenis@mail.ru>
2023-10-20 00:20:55 +0300

    Реализация передачи данных из БД в визуал + Проверка имён игроков

---
+++ GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+
+namespace Laba
+{
+    struct Leaderboard
+    {
+        internal string Nick;
+        internal int Wins;
+    }
+    internal class GameLogic
+    {       
+        public static void Start(int Index, out Leaderboard Player)
+        {
+            Data.SetTop(Index, out Player.Nick, out Player.Wins);
+        }
+        public static void Check(out bool Key)
+        {
+            if (String.IsNullOrWhiteSpace(Save.Player1) || String.IsNullOrWhiteSpace(Save.Player2)
+                || (Save.Player1.Length < 3 || Save.Player1.Length > 30) || (Save.Player2.Length < 3 || Save.Player2.Length > 30)
+                || Save.Player1 == Save.Player2)
+            {
+                Key = false;
+            }
+            else
+            {
+                Key = true;
+            }
+        }
+    }
+}
