8bfcf29d62068ae86a768458cc620b47a4d9a7f4
Денис <valchukdenis@mail.ru>
2023-10-20 00:24:30 +0300

    Реализация открытия стартовой страницы

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace Laba
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Start (object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new Uri("/Pages/StartPage.xaml", UriKind.Relative));
+        }
+    }
+}
