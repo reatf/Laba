5def3b8388443ad80f73c04514ee9c99d6576c9f
Денис <valchukdenis@mail.ru>
2023-10-20 00:26:03 +0300

    Реализация Стартовой страницы, а также страницы присвоения имён

---
+++ Pages/PlayersNicksPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Laba
+{
+    public partial class PlayersNicksPage : Page
+    {
+        public PlayersNicksPage()
+        {
+            InitializeComponent();
+        }
+        private async void HintsShow(object sender, RoutedEventArgs e)
+        {
+            MainAnimation(500);
+            await Task.Delay(1100);
+            HintAnimation(-150);
+            EnterButtonAnimation(90);
+        }
+        private async void MainAnimation(int Vector)
+        {
+            UIElement[] Main = { PlayersNicks, Player1Nick, Player2Nick, PlayersHint, EnterButton, ErrorName };
+            TranslateTransform TransformMain = new();
+            DoubleAnimation Position = new(0, Vector, TimeSpan.FromSeconds(1));
+
+            for (int i = 0; i < Main.Length; i++)
+            {
+                Main[i].RenderTransform = TransformMain;
+                TransformMain.BeginAnimation(TranslateTransform.YProperty, Position);
+            }
+
+            await Task.Delay(1000);
+            for (int i = 0; i < Main.Length; i++)
+            {
+                Main[i].RenderTransform = TransformMain;
+                TransformMain.BeginAnimation(TranslateTransform.YProperty, null);
+            }
+            for (int i = 0; i < Main.Length; i++)
+            {
+                Canvas.SetTop(Main[i], Canvas.GetTop(Main[i]) + Vector);
+            }
+        }
+        private async void HintAnimation(int Vector)
+        {
+            TranslateTransform TransformLeft = new();
+            DoubleAnimation Position = new(0, Vector, TimeSpan.FromSeconds(1));
+
+            PlayersHint.RenderTransform = TransformLeft;
+            TransformLeft.BeginAnimation(TranslateTransform.XProperty, Position);
+            await Task.Delay(1000);
+
+            TransformLeft.BeginAnimation(TranslateTransform.XProperty, null);
+            Canvas.SetLeft(PlayersHint, Canvas.GetLeft(PlayersHint) + Vector);
+        }
+        private async void EnterButtonAnimation(int Vector)
+        {
+            TranslateTransform TransformTop = new();
+            DoubleAnimation Position = new(0, Vector, TimeSpan.FromSeconds(1));
+
+            EnterButton.RenderTransform = TransformTop;
+            TransformTop.BeginAnimation(TranslateTransform.YProperty, Position);
+            await Task.Delay(1000);
+
+            TransformTop.BeginAnimation(TranslateTransform.YProperty, null);
+            Canvas.SetTop(EnterButton, Canvas.GetTop(EnterButton) + Vector);
+        }
+        private void EnterButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            bool Key;
+            Save.Player1 = Player1Nick.Text;
+            Save.Player2 = Player2Nick.Text;
+            GameLogic.Check(out Key);
+            if(Key)
+            {
+                StartGame();
+            }
+            else
+            {
+                Error();
+            }
+        }
+        public async void StartGame()
+        {
+            ErrorName.Visibility = Visibility.Hidden;
+            HintAnimation(150);
+            EnterButtonAnimation(-90);
+            await Task.Delay(1100);
+            MainAnimation(-500);
+        }
+        public void Error()
+        {
+            ErrorName.Visibility = Visibility.Visible;
+        }
+        public void BoardAnimation(int Vector)
+        {
+
+        }
+    }
+}
---
+++ Pages/StartPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace Laba
+{
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+        private void Leaderboard(object sender, RoutedEventArgs e)
+        {
+            Label[] TopNicks = { TopPlayer1, TopPlayer2, TopPlayer3, TopPlayer4, TopPlayer5,
+                                 TopPlayer6, TopPlayer7, TopPlayer8, TopPlayer9, TopPlayer10 }; // Разобраться
+
+            Label[] TopScores = { TopScore1, TopScore2, TopScore3, TopScore4, TopScore5,
+                                  TopScore6, TopScore7, TopScore8, TopScore9, TopScore10 };
+
+            for (int Index = 0; Index < TopNicks.Length; Index++)
+            {
+                GameLogic.Start(Index, out Leaderboard Player);
+                if (Player.Wins == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    TopNicks[Index].Content = Player.Nick;
+                    TopScores[Index].Content = Player.Wins;
+                }
+
+            }
+        }
+        private void Exit(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private async void StartGame(object sender, MouseButtonEventArgs e)
+        {
+            UIElement[] ElementsRight = { StartButton, LoadSaveButton, ExitButton };
+            UIElement[] ElementsLeft = { TopPlayers,
+                                         TopPlayer1, TopPlayer2, TopPlayer3, TopPlayer4, TopPlayer5, TopPlayer6, TopPlayer7, TopPlayer8, TopPlayer9, TopPlayer10,
+                                         TopScore1, TopScore2, TopScore3, TopScore4, TopScore5, TopScore6, TopScore7, TopScore8, TopScore9, TopScore10 };
+
+            for (int Index = 0; Index < ElementsRight.Length; Index++) StartAnimation(ElementsRight[Index], 3000);
+            for (int Index = 0; Index < ElementsLeft.Length; Index++) StartAnimation(ElementsLeft[Index], -3000);
+            await Task.Delay(1000);
+
+            NavigationService.RemoveBackEntry();
+            NavigationService.Navigate(new Uri("/Pages/PlayersNicksPage.xaml", UriKind.Relative));
+        }
+        private static void StartAnimation(UIElement Element, int Position)
+        {
+            TranslateTransform Transform = new();
+            Element.RenderTransform = Transform;
+            DoubleAnimation Xposition = new(0, Position, TimeSpan.FromSeconds(1));
+            Transform.BeginAnimation(TranslateTransform.XProperty, Xposition);
+        }
+    }
+}
