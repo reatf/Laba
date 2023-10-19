dae1e191350663ac9a5b6bb9351755e279942e71
Денис <valchukdenis@mail.ru>
2023-10-20 00:14:30 +0300

    Реализация получения информации для таблицы лидеров

---
+++ Data.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Laba
+{
+    internal class Data
+    {
+        internal static void SetTop(int Index, out string? TopNicks, out int TopWins)
+        {
+            MySqlConnection Connection = new("Server=localhost;Port=3306;Database=reversi_schema;Uid=root;Pwd=1324");
+            Connection.Open();
+            MySqlCommand Command = new("Select NickName, Wins from leaderboard order by Wins DESC Limit @Index,1;", Connection);
+            Command.Parameters.AddWithValue("@Index", Index);
+            MySqlDataReader Reader = Command.ExecuteReader();
+            Reader.Read();
+            if (Reader.HasRows)
+            {
+                TopNicks = Reader["NickName"].ToString();
+                TopWins = int.Parse(Reader["Wins"].ToString());
+                Connection.Close();
+            }
+            else
+            {
+                TopNicks = "";
+                TopWins = 0;
+            }
+        }
+    }
+}
