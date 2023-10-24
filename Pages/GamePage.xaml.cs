846c7a5eb41d97c1a258721c45280d5e7ee2ef9b
Денис <valchukdenis@mail.ru>
2023-10-24 13:26:24 +0300

    Реализация визуальных методов механики реверси

--- Pages/GamePage.xaml.cs
+++ Pages/GamePage.xaml.cs
@@ -24,7 +24,9 @@ namespace Laba
         public GamePage()
         {
             InitializeComponent();
+            FoundMoves();
         }
+
         private async void MainAnimation(int Column, int Row)
         {
             const int Change = 42;
@@ -46,7 +48,7 @@ namespace Laba
             Positions[Column, Row].BeginAnimation(WidthProperty, WidthAnimation);
             Transform.BeginAnimation(TranslateTransform.XProperty, LeftAnimation);
             await Task.Delay(200);
-            if (Positions[Column, Row].Source == WhiteChip.Source)
+            if (Positions[Column, Row].Source.ToString() == WhiteChip.Source.ToString())
             {
 
                 Positions[Column, Row].Source = BlackChip.Source;
@@ -299,6 +301,59 @@ namespace Laba
             foreach(var Coordinates in AvailableMoves)
             {
                 Positions[Coordinates.Row, Coordinates.Column].Visibility = Visibility.Visible;
+            }
+        }
+
+        private void ChipPlace(object sender, MouseButtonEventArgs e)
+        {
+            Image GetImage = (Image) sender;
+            if(GetImage.Source.ToString() == HintMove.Source.ToString())
+            {
+            Image[,] Positions = { { A1, B1, C1, D1, E1, F1, G1, H1 },
+                                   { A2, B2, C2, D2, E2, F2, G2, H2 },
+                                   { A3, B3, C3, D3, E3, F3, G3, H3 },
+                                   { A4, B4, C4, D4, E4, F4, G4, H4 },
+                                   { A5, B5, C5, D5, E5, F5, G5, H5 },
+                                   { A6, B6, C6, D6, E6, F6, G6, H6 },
+                                   { A7, B7, C7, D7, E7, F7, G7, H7 },
+                                   { A8, B8, C8, D8, E8, F8, G8, H8 } };
+                for (int Row = 0; Row < Positions.GetLength(0); Row++)
+                {
+                    for (int Column = 0; Column < Positions.GetLength(1); Column++)
+                    {
+                        if (Positions[Row, Column].Name == GetImage.Name)
+                        {
+                            GameLogic Logic = new();
+                            List<Coordinates> Reversi = Logic.GetReversiChips(Row, Column, out int Turn);
+                            if (Turn == 1)
+                            {
+                                Positions[Row, Column].Source = new BitmapImage(new Uri("/Images/WhiteChip.png", UriKind.Relative));
+                                foreach (var Position in Reversi)
+                                {
+                                    MainAnimation(Position.Row, Position.Column);
+                                }
+                            }
+                            else
+                            {
+                                Positions[Row, Column].Source = new BitmapImage(new Uri("/Images/BlackChip.png", UriKind.Relative));
+                                foreach (var Position in Reversi)
+                                {
+                                    MainAnimation(Position.Row, Position.Column);
+                                }
+                            }
+                        }
+                    }
+                }
+                for (int Row = 0; Row < Positions.GetLength(0); Row++)
+                {
+                    for (int Column = 0; Column < Positions.GetLength(1); Column++)
+                    {
+                        if (Positions[Row,Column].Source.ToString() == HintMove.Source.ToString())
+                        {
+                            Positions[Row, Column].Visibility = Visibility.Hidden;
+                        }
+                    }
+                }
             }
         }
     }
