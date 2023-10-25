e30dc317cf7799de049cb74ce0f02f493fca52cf
Денис <valchukdenis@mail.ru>
2023-10-25 18:16:02 +0300

    Реализация методо по передачи итогов партии в таблицу лидеров

--- Data.cs
+++ Data.cs
@@ -16,13 +16,46 @@ namespace Laba
             {
                 TopNicks = Reader["NickName"].ToString();
                 TopWins = int.Parse(Reader["Wins"].ToString());
-                Connection.Close();
             }
             else
             {
                 TopNicks = "";
                 TopWins = 0;
             }
+            Connection.Close();
+        }
+        internal static void SetWinner(string Winner)
+        {
+            bool PlayerExist;
+            MySqlConnection Connection = new("Server=localhost;Port=3306;Database=reversi_schema;Uid=root;Pwd=1324");
+            Connection.Open();
+            MySqlCommand Command = new MySqlCommand("select * from leaderboard where NickName = @NickName;", Connection);
+            Command.Parameters.AddWithValue("@NickName", Winner);
+            MySqlDataReader Reader = Command.ExecuteReader();
+            if (Reader.HasRows)
+            {
+                PlayerExist = true;
+            }
+            else
+            {
+                PlayerExist = false;
+            }
+            Connection.Close();
+            Connection.Open ();
+
+            if (PlayerExist)
+            {
+                Command = new MySqlCommand("Update leaderboard set Wins = Wins + 1 where Nickname = @NickName;", Connection);
+                Command.Parameters.AddWithValue("@NickName", Winner);
+                Command.ExecuteNonQuery();
+            }
+            else
+            {
+                Command = new MySqlCommand("insert into leaderboard (NickName, Wins) value (@NickName, 1);", Connection);
+                Command.Parameters.AddWithValue("@NickName", Winner);
+                Command.ExecuteNonQuery();
+            }
+            Connection.Close();
         }
     }
 }
