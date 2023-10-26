6b3f31b803791a07d9ecbd664292cc39555b7e34
Rus <ruslan.khasanov.03@mail.ru>
2023-10-27 00:22:02 +0300

    Реализация нового экземпляра

--- Coordinates.cs
+++ Coordinates.cs
@@ -8,12 +8,19 @@ namespace Laba
 {
     internal class Coordinates
     {
+        public int Color { get; set; }
         public int Column { get; set; }
         public int Row { get; set; }
         public Coordinates(int Row, int Column)
         {
             this.Column = Column;
             this.Row = Row;
+        }
+        public Coordinates(int Row, int Column, int Color)
+        {
+            this.Column = Column;
+            this.Row = Row;
+            this.Color = Color;
         }
     }
 }
